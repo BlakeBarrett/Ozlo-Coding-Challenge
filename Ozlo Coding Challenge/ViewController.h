023161caff3d5838bf4d0ee0a10fd9277b99c374
Blake Barrett <blake@blakebarrett.net>
2016-06-11 14:20:13 -0700

    Initial Commit

---
+++ Ozlo Coding Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ozlo Coding Challenge
+//
+//  Created by Blake Barrett on 6/11/16.
+//  Copyright © 2016 Blake Barrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ozlo Coding Challenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ozlo Coding Challenge
+//
+//  Created by Blake Barrett on 6/11/16.
+//  Copyright © 2016 Blake Barrett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
