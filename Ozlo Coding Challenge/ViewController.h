9331a389966bfb2c9ecbf2a36223f29660575cd6
Blake Barrett <blake@blakebarrett.net>
2016-06-11 17:23:38 -0700

    Everything but showing the selected image works!

--- Ozlo Coding Challenge/ViewController.h
+++ Ozlo Coding Challenge/ViewController.h
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
+@property (weak, nonatomic) IBOutlet UICollectionView *imagesCollectionView;
 
 @end
-
--- Ozlo Coding Challenge/ViewController.m
+++ Ozlo Coding Challenge/ViewController.m
@@ -14,14 +14,113 @@
 
 @implementation ViewController
 
+static NSString * const reuseIdentifier = @"ozloImageCellReuseIdentifier";
+
+NSArray *imageUrlStrings;
+NSMutableArray *images;
+
+- (id) init {
+    self = [super init];
+    
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    images = [NSMutableArray new];
+    imageUrlStrings = [[NSArray alloc] initWithObjects: @"https://images.njck.co/img?q=jack://i/ios-challenge/img.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img1.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img2.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img4.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img5.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img6.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img7.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img8.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img9.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img10.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img11.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img12.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img13.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img14.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img15.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img16.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img17.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img18.jpeg",
+              @"https://images.njck.co/img?q=jack://i/ios-challenge/img19.jpeg", nil];
+    
+    [self.imagesCollectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    self.imagesCollectionView.delegate = self;
+    self.imagesCollectionView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (UIImageView *)imageViewForIndexPath:(NSIndexPath *)indexPath {
+    NSInteger index = indexPath.row;
+    
+    UIImageView *imageView;
+    if (index >= images.count) {
+        NSString *imageURLString = imageUrlStrings[index];
+        NSURL *imageURL = [NSURL URLWithString:imageURLString];
+        NSData *imageData = [[NSData alloc] initWithContentsOfURL:imageURL];
+        UIImage *image = [UIImage imageWithData:imageData];
+        
+        imageView = [[UIImageView alloc] initWithImage:image];
+        [imageView setContentMode:UIViewContentModeScaleAspectFit];
+        
+        images[index] = imageView;
+    } else {
+        imageView = images[index];
+    }
+
+    return imageView;
+}
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return imageUrlStrings.count;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return imageUrlStrings.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    UIImageView *imageView = [self imageViewForIndexPath:indexPath];
+    cell.backgroundView = imageView;
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewFlowDelegate>
+
+-(CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *) collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UIImageView *imageView = [self imageViewForIndexPath:indexPath];
+    
+    float aspectRatio = imageView.image.size.width / imageView.image.size.height;
+    
+    CGFloat width = (collectionView.frame.size.width / 2.1);
+    CGFloat height = width / aspectRatio;
+    
+    CGSize size = CGSizeMake(width, height);
+    return size;
+}
+
+
+#pragma mark <UICollectionViewDelegate>
+
+- (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    UIImageView *imageView = [self imageViewForIndexPath:indexPath];
 }
 
 @end
